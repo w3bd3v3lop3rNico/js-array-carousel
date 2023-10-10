49a11ab034ae3c1a314cf499fea0ae5dc7fdc8ef
Nicola Tabai <nicola.tabai98@gmail.com>
2023-10-10 17:47:42 +0200

    style none to array imgs

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-console.log("ciao")
+
+// - generare un array contenente la lista delle immagini
+const imgs = [
+    "./img/01.webp",
+    "./img/02.webp",
+    "./img/03.webp",
+    "./img/04.webp",
+    "./img/05.webp"
+]
+const caroselloDOMElement = document.querySelector('.container')
+const arrayLength = imgs.length
+
+// - Rimuovere il markup statico
+//         - inserire tutte le immgini dinamicamente
+//             - serviamoci dell'array per generare un ciclo for
+
+for ( let i = 0; i < arrayLength; i++ ) {
+    let immagineCorrente = imgs[i]
+    console.log(i, immagineCorrente)
+    let imgString =`
+    <div class="d-none">
+        <img src="${immagineCorrente}"></img>
+    </div>
+    `
+    caroselloDOMElement.innerHTML += imgString
+}
+//         - il ciclo dovrà concatenare un template literal
+
+
+//         - tutte le immagini dovranno essere nascoste, tranne la prima che avrà una classe display block
+
+
+
