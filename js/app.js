5817516a816f56fc6ef49a8e5acf5ffa9a70090e
Nicola Tabai <nicola.tabai98@gmail.com>
2023-10-11 15:31:31 +0200

    Added class active to  index image 0 - style buttons - Fixed html

--- js/app.js
+++ js/app.js
@@ -7,27 +7,74 @@ const imgs = [
     "./img/04.webp",
     "./img/05.webp"
 ]
-const caroselloDOMElement = document.querySelector('.container')
-const arrayLength = imgs.length
+
+// - Richiamare elemento dal DOM
+const caroselloDOMElement = document.querySelector('.carousel')
+
+// - Trovare lunghezza dell'array
+// const arrayLength = imgs.length
 
 // - Rimuovere il markup statico
 //         - inserire tutte le immgini dinamicamente
 //             - serviamoci dell'array per generare un ciclo for
 
-for ( let i = 0; i < arrayLength; i++ ) {
-    let immagineCorrente = imgs[i]
-    console.log(i, immagineCorrente)
-    let imgString =`
-    <div class="d-none">
-        <img src="${immagineCorrente}"></img>
-    </div>
-    `
-    caroselloDOMElement.innerHTML += imgString
-}
-//         - il ciclo dovrà concatenare un template literal
+for ( let i = 0; i < imgs.length; i++ ) {
+    let immagineCorrenteSrc = imgs[i]
+    // let immagine1 = imgs[0]
+    console.log(i, immagineCorrenteSrc)
+    // console.log(immagine1)
 
+//        - il ciclo dovrà concatenare un template literal
+    let imgString =`
+        <img src="${immagineCorrenteSrc}" class = "img-item"></img>
+    `
+    console.log(imgString)
+    caroselloDOMElement.innerHTML += imgString
+    // ESPERIMENTI MIEI
+    // caroselloDOMElement.classList.add('d-none')
+    // const caroselloImgDOMElement = document.querySelector('image')
+    // let active = caroselloImgDOMElement[0]
+    // active.classList.add('d-block')
+    // carosello1ImgDOMElement.classList.add('d-block')
+}
 
 //         - tutte le immagini dovranno essere nascoste, tranne la prima che avrà una classe display block
+
+// recupero dal DOM tutte le immagini stampate nel ciclo
+const caroselloImgDOMElements = document.querySelectorAll('.img-item');
+console.log(caroselloImgDOMElements);
+
+// memorizzo lo stato del carosello (indice slide attiva)
+let currentIndex = 0;
+
+// aggiungo la classe active alla slide attiva
+let currentSlide = caroselloImgDOMElements[currentIndex];
+currentSlide.classList.add('active')
+
+// recupero dal DOM le frecce
+
+const arrowTop = document.querySelector('.carousel .arrow-top');
+
+const arrowBottom = document.querySelector('.carousel .arrow-bottom');
+
+console.log(arrowTop, arrowBottom)
+// creo l'event listener per ogni freccia
+arrowBottom.addEventListener('click', function() {
+    console.log("click arrow bottom")
+
+    // assegnare alla slide successiva la classe active
+    
+    
+})
+
+arrowTop.addEventListener('click', function(){
+    console.log('click arrow top')
+
+    // assegnare alla classe precedente la classe active
+
+})
+
+
 
 
 
